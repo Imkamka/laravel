50264d52dae227305d6bd6223cea18190a95d586
ASf4646 <islmbd994@gmail.com>
2024-08-02 15:43:16 +0500

    second commit

--- resources/views/Admin/includes/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-<script src="{{ asset('assets/js/popper.js') }}"></script>
-<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/js/main.js') }}"></script>
-</body>
-
-</html>
--- resources/views/Admin/layout/app.blade.php
+++ resources/views/Admin/layout/app.blade.php
@@ -1,13 +1,36 @@
-@include('Admin.includes.header')
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="wrapper d-flex align-items-stretch">
-    @include('Admin.layout.sidebar')
-    <!-- Page Content  -->
-    <div id="content" class="p-4 p-md-5">
-        @include('Admin.layout.navbar')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rafay diaries and farms</title>
 
-        @yield('content')
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+</head>
+
+<body>
+
+    <div class="wrapper d-flex align-items-stretch">
+        @include('Admin.includes.sidebar')
+        <!-- Page Content  -->
+        <div id="content" class="p-4 p-md-5">
+            @include('Admin.includes.navbar')
+
+            @yield('content')
+        </div>
     </div>
-</div>
 
-@include('Admin.includes.footer')
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+</body>
+
+</html>
