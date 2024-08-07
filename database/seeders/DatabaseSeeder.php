6615022ddb1bd9cc3d0e813e142689ff9be03d51
AsifKhan <132731011+ASf4646@users.noreply.github.com>
2024-08-07 20:58:58 +0500

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(1)->create();
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
